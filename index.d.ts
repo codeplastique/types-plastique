8d54f6c7d0157aa8a54419fac3b569e6656063b8
codeplastique <rockets@bk.ru>
2019-09-06 21:07:44 +0300

    init

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,12 @@
-import Bean from "reactive-plastique/annotations/Bean";
+import Bean from "plastique/annotation/Bean";
+import Autowired from "plastique/annotation/Autowired";
+import Component from "plastique/annotation/Component";
+import Configuration from "plastique/annotation/Configuration";
+import Listener from "plastique/annotation/Listener";
+import Reactive from "plastique/annotation/Reactive";
+
+import App from "plastique/App";
+import EventManager from "plastique/EventManager";
+import HttpRequest from "plastique/HttpRequest";
+import I18n from "plastique/I18n";
+import RestService from "plastique/RestService";
